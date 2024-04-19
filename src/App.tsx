b1a128ed20015fcce4d0e0908a6e7d3f2077f025
Cloviski <claudioribeiro.j@hotmail.com>
2024-04-19 16:41:40 -0300

    First component WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="relative h-screen w-screen bg-cover"
+      style={{ backgroundImage: "url('/assets/sales.jpg')" }}
+    >
+      <div className="absolute inset-0 backdrop-blur"></div>
+      <div className="absolute inset-0 bg-gradient-to-br from-neutral-300 to-green-700 opacity-80"></div>
+      <div className="absolute flex h-screen w-screen  items-center justify-center gap-x-20">
+        <div className="w-1/4">
+          <img
+            className="from-glass shadow-lg w-48 rounded-3xl border-solid border border-border bg-gradient-to-br to-transparent p-8 backdrop-blur"
+            src="/assets/brain.png"
+            alt="cerébro logo"
+          />
+          <h1 className="text-4xl font-bold text-white">
+            Desvende os segredos do sucesso em vendas escrito por um veterano
+            com mais de 10 anos de experiência, explorando diversas áreas do
+            negócio para alcançar a alta performance.
+          </h1>
+          <span></span>
+          <button className="mt-8 w-full whitespace-nowrap rounded-l-full rounded-r-full bg-gradient-to-r from-green-700 to-sell px-6 py-3 font-sans font-extrabold uppercase text-white">
+            Quero ser de alta performance
+          </button>
+        </div>
+        <img className="h-96 rounded-3xl" src="/assets/seller.jpg" alt="pdf" />
+      </div>
     </div>
   );
 }
 
-export default App;
+/** 
+ * 
+ * 
+ *   
+ *   
+ *  <div className="to-green-700 relative flex h-screen w-screen items-center justify-center bg-gradient-to-br from-stone-800">
+      <img
+        src="/assets/sales.jpg"
+        className="h-screen w-full object-none opacity-10 blur filter"
+        alt=""
+      >
+      </img>
+    </div>
+ * */
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        sell: "#7ed956",
+        glass: "#ffffff1a",
+        border: "#ffffff33",
+      },
+    },
+  },
+  plugins: [],
+};
