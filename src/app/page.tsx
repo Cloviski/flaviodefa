0d780112d41af08628709d3c0e61662935fe2ae0
Cloviski <claudioribeiro.j@hotmail.com>
2024-04-29 17:55:51 -0300

    Added writer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,50 @@
 import Image from "next/image";
 import Link from "next/link";
+import Ebook from "./components/ebook";
 
 export default function Home() {
   return (
-    <main>
-    <div
-      className="relative h-screen w-screen bg-cover"
-      style={{ backgroundImage: "url('/assets/sales.jpg')" }}
-    >
-      <div className="absolute inset-0 backdrop-blur-lg"></div>
-      <div className="absolute inset-0 bg-gradient-to-br from-black to-green-700 opacity-80"></div>
-      <div className="absolute flex flex-col md:flex-row h-screen w-screen items-center justify-center gap-x-40">
-        <div className="flex w-1/4 flex-col gap-8">
-          <div className="flex w-96 items-center gap-3">
-            <Image width={96} height={96} src="/assets/brain.png" alt="cerebro logo" />
-            <h2 className="text-4xl font-handwritten text-sell">
+    <main className="overflow-x-hidden">
+      <Ebook />
+      <div className="relative flex justify-center items-center w-full bg-gradient-to-b h-[800px] bg-black">
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black"></div>
+        <div className="w-1/4 h-5/6 bg-[#ffffff1a] backdrop-blur shadow-2xl border border-solid border-[#ffffff22] rounded-3xl ml-52">
+          <div className="p-8">
+            <span className="text-sell">Conheça o escritor</span>
+            <h2 className="text-4xl font-handwritten text-white my-4">
               Flávio Sousa
             </h2>
+            <p className="text-neutral-400 mb-4">
+              Meu nome é Flávio Ribeiro de Sousa, nascido em Salvador, Bahia.
+              Comecei minha jornada como vendedor de rua e, eventualmente,
+              trabalhei como frentista em um posto de gasolina. Descobri minha
+              paixão pela música percussiva e viajei com a banda juvenil Olodum.
+            </p>
+            <p className="text-neutral-400 mb-4">
+              Tornei-me pai aos 19 anos e, devido à incerteza da música como
+              carreira, empreendi no ramo de alimentação, embora tenha
+              enfrentado desafios. Mais tarde, entrei na indústria de peças
+              automotivas como motorista, visando me tornar vendedor. Após seis
+              anos de esforço, alcancei meu objetivo e vendi mais de 30 milhões
+              em vendas, adquirindo experiência valiosa.
+            </p>
+            <p className="text-neutral-400">
+              Em 2021, descobri o mercado digital e me apaixonei pela escala que
+              oferece. Atualmente, ensino outras pessoas a vender transformação
+              e se destacar no mercado digital, compartilhando meu conhecimento
+              e experiência. Minha jornada é um testemunho de determinação e
+              paixão que levaram a transformações significativas em minha vida.
+            </p>
           </div>
-          <h1 className="text-3xl font-bold text-white">
-            Desvende os segredos do sucesso em vendas escrito por um veterano
-            com mais de 10 anos de experiência, explorando diversas áreas do
-            negócio para alcançar a alta performance.
-          </h1>
-          <Link href="https://pay.kiwify.com.br/SDaIhL6">
-            <button className="w-full rounded-l-full rounded-r-full bg-gradient-to-r from-green-700 to-sell px-6 py-3 font-sans font-extrabold uppercase text-white">
-              Quero ser de alta performance
-            </button>
-          </Link>
-            
         </div>
-        <img className="h-1/2 rounded-3xl shadow-2xl shadow-green-900" src="/assets/seller.jpg" alt="pdf" />
+        <Image
+          className=""
+          width={800}
+          height={0}
+          src="/assets/flaviob&w.png"
+          alt="flavio"
+        />
       </div>
-    </div>
     </main>
   );
 }
