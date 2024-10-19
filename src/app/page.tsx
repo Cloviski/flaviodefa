caf2cc797ce7631f8f1bbf33fc53f2ffcaa3ba85
Cloviski <claudioribeiro.j@hotmail.com>
2024-10-19 13:37:32 -0300

    Back to normal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-"use client"
-
-import { useEffect } from 'react';
 import Ebook from "./components/ebook";
 import AboutMe from "./components/aboutme";
 import Buy from "./components/buy";
@@ -10,17 +7,6 @@ import Topics from "./components/topics";
 import Audience from "./components/audience";
 
 export default function Home() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://d335luupugsy2.cloudfront.net/js/loader-scripts/3864883d-8038-4215-8d87-1091a4fe9be7-loader.js';
-    script.async = true;
-    document.body.appendChild(script);
-    
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <main className="overflow-x-hidden">
       <Ebook />
