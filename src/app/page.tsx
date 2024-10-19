ff17727a3ab9bc8ff1476f999800bcb5c785e906
Cloviski <claudioribeiro.j@hotmail.com>
2024-10-19 11:32:54 -0300

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect } from 'react';
 import Ebook from "./components/ebook";
 import AboutMe from "./components/aboutme";
 import Buy from "./components/buy";
@@ -7,6 +10,17 @@ import Topics from "./components/topics";
 import Audience from "./components/audience";
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://d335luupugsy2.cloudfront.net/js/loader-scripts/3864883d-8038-4215-8d87-1091a4fe9be7-loader.js';
+    script.async = true;
+    document.body.appendChild(script);
+    
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <main className="overflow-x-hidden">
       <Ebook />
@@ -19,3 +33,5 @@ export default function Home() {
     </main>
   );
 }
+
+
